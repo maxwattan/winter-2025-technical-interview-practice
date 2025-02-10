db45eeeeaf575aa178f3075b5ad8242486a04eec
MaxWattan <mcwattanac@gmail.com>
2025-02-10 13:02:20 -0500

    passed palidrome

--- lesson01-tdd/problem.js
+++ lesson01-tdd/problem.js
@@ -11,7 +11,31 @@
  * @returns {boolean} - True if the string is a palindrome, false otherwise.
  */
 function isPalindrome(str) {
-  // your code here
-}
+  // input => string 
+  // output => if the characters reads same foward and backwards return true else return false
+
+  //initialize variable and set it to the last element
+
+  let j = str.length-1;
+//string length goes half way
+  for (let i=0; i < str.length/2; i++){
+    //check first and last then loop to the next if true
+    if (str[i] !== str[j]) {
+      return false
+    } 
+  //move on to the next element
+    j--
+     //if all matches return true
+      return true
+    }
+  }
+  console.log(isPalindrome("mom"))
+  console.log(isPalindrome("pop"))
+  console.log(isPalindrome("121"))
+  console.log(isPalindrome("123"))
+  console.log(isPalindrome("!.!"))
+  console.log(isPalindrome(" . "))
+
+
 
 module.exports = isPalindrome;
